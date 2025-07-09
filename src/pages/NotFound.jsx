ad9371f54710b517bec79d6ea915053788dd06d3
SandrinePichla <sandrinepichla@hotmail.com>
2025-07-09 15:36:57 +0200

    Suppression des logos et ajout de nouvelles bannières, mise à jour de la page NotFound avec un style et un message améliorés

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div className="not-found">
-      <h1>Erreur 404</h1>
-      <p>Page not found</p>
-      <Link to="/">Retourner à l’accueil</Link>
+    <div className="not_found">
+      <h1 className="not_found_title">404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d’accueil</Link>
     </div>
   );
 }
